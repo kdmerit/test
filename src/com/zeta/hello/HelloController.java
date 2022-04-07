831b597f58ecb870a1b541e2e7584854eeea0ebf
zetamind <kdmerit@naver.com>
2022-04-07 17:15:37 +0900

    1번 Mark as Merged

--- src/com/zeta/hello/HelloController.java
+++ src/com/zeta/hello/HelloController.java
@@ -3,6 +3,6 @@ package com.zeta.hello;
 public class HelloController {
 	public static void main(String[] args) {
 		System.out.println("Hello");
-		System.out.println("Welcome");
+		System.out.println("Welcome ");
 	}
 }
