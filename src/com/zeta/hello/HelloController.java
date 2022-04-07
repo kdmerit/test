963a3f63949844bcc6def4bde244fcb63d471dea
zetamind <kdmerit@naver.com>
2022-04-07 17:12:07 +0900

    1번이다

--- src/com/zeta/hello/HelloController.java
+++ src/com/zeta/hello/HelloController.java
@@ -3,6 +3,6 @@ package com.zeta.hello;
 public class HelloController {
 	public static void main(String[] args) {
 		System.out.println("Hello");
-		System.out.println("Hi There");
+		System.out.println("Welcome");
 	}
 }
