9a3c097ec40e0f252a45c8b84d7b7f138fefecac
zetamind <kdmerit@naver.com>
2022-04-08 13:28:59 +0900

    커밋 1

--- src/com/zeta/hello/HelloController.java
+++ src/com/zeta/hello/HelloController.java
@@ -2,12 +2,8 @@ package com.zeta.hello;
 
 public class HelloController {
 	public static void main(String[] args) {
-		System.out.println("Hello");
-		System.out.println("Hi There");
-		System.out.println("Go Away");
-		System.out.println("222222");
-		System.out.println("1111112222");
-		System.out.println("1¹ø");
+		System.out.println("Hello 1");
+		
 		
 	}
 }
