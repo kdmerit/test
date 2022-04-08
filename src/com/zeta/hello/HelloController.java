958000def47725dbbdc1d6e4a05a6ad322ef6c81
zetamind <kdmerit@naver.com>
2022-04-08 16:42:14 +0900

    Revert "Revert "test3333""
    This reverts commit 7508c42c9b9660aaf4bc3cd08aa9a1f01a05e4e2.

--- src/com/zeta/hello/HelloController.java
+++ src/com/zeta/hello/HelloController.java
@@ -6,7 +6,7 @@ public class HelloController {
 		System.out.println("Hello 2");
 		System.out.println("Hello 3");
 		System.out.println("Hello 4 new Branch");
-		System.out.println("test");
+		System.out.println("test111222");
 		
 		
 	}
