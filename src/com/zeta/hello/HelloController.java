43f10fed84dc84404a3ad34c7e6ad2c137935948
zetamind <kdmerit@naver.com>
2022-04-13 09:37:41 +0900

    1121212

--- src/com/zeta/hello/HelloController.java
+++ src/com/zeta/hello/HelloController.java
@@ -3,13 +3,6 @@ package com.zeta.hello;
 public class HelloController {
 	public static void main(String[] args) {
 		System.out.println("Hello 1");
-		System.out.println("Hello 2");
-		System.out.println("Hello 3");
-		System.out.println("Hello 4 new Branch");
-		System.out.println("test111222");
-		System.out.println("branch user1_a »ý¼º");
-		System.out.println("user1 gogo");
-		System.out.println("bbb1234");
 		
 		
 	}
