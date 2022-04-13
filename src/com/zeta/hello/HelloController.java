fd2a15ce9235db12c1f375de37fdebd8cb4c0eec
zetamind <kdmerit@naver.com>
2022-04-13 10:01:56 +0900

    Hello 변경, Hi 추가

--- src/com/zeta/hello/HelloController.java
+++ src/com/zeta/hello/HelloController.java
@@ -3,6 +3,9 @@ package com.zeta.hello;
 public class HelloController {
 	public static void main(String[] args) {
 		System.out.println("Hello 1");
+		System.out.println("Hello 2");
+		System.out.println("Hello 3");
+		System.out.println("Hello 4");
 		
 		
 	}
---
+++ src/com/zeta/hi/HiController.java
@@ -0,0 +1,7 @@
+package com.zeta.hi;
+
+public class HiController {
+	public static void main(String[] args){
+		System.out.println("Hi 1");
+	}
+}
