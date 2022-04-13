4ff1acd4a55c3730ca1a8095f8c98f566e491834
zetamind <kdmerit@naver.com>
2022-04-13 14:30:11 +0900

    작업시작

--- src/com/zeta/hello/HelloController.java
+++ src/com/zeta/hello/HelloController.java
@@ -3,15 +3,5 @@ package com.zeta.hello;
 public class HelloController {
 	public static void main(String[] args) {
 		System.out.println("Hello 1");
-		System.out.println("Hello 2");
-		System.out.println("Hello 3");
-		System.out.println("Hello 4");
-		System.out.println("Hello 5");
-		System.out.println("Hello 666");
-		System.out.println("Hello test2 111");
-		System.out.println("Hello test2 222");
-		System.out.println("Hello 777");
-		System.out.println("Hello test1 111");
-		System.out.println("Hello test1 111");
 	}
 }
