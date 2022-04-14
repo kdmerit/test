a99136ce1ac35dd2956215ed43847c48ad7892c3
zetamind <kdmerit@naver.com>
2022-04-14 09:23:43 +0900

    사용자 222

--- src/com/zeta/hi/HiController.java
+++ src/com/zeta/hi/HiController.java
@@ -2,10 +2,6 @@ package com.zeta.hi;
 
 public class HiController {
 	public static void main(String[] args){
-		System.out.println("Hi 1");
-		System.out.println("Hi 2");
-		System.out.println("Hi 3 test2");
-		System.out.println("Hi test2 111");
-		System.out.println("Hi test2 222");
+		System.out.println("»ç¿ëÀÚ222");
 	}
 }
